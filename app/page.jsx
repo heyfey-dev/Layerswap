f5f48818dec97f1e4477ef4220d582ba018556cf
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-12 21:08:41 +0100

    installed a package & wring the html for the container

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,16 @@ import Logo from "./components/Logo";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-l from-[#101429] via-[#2f1136] to-[#101429] h-screen w-full">
+    <main className="bg-gradient-to-l from-[#0c1526] via-[#2f1136] to-[#0c1526] h-screen w-full">
       <div>
         <Logo />
         <h2>Layerswap</h2>
+      </div>
+
+      <div className="container mx-auto bg-[#0c1526] w-[40%] h-72 ">
+        <div>
+          
+        </div>
       </div>
     </main>
   );
