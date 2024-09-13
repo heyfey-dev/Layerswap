345ef2f25cee5d27ca2b5da0ab55605edabd7777
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-13 13:33:43 +0100

    done with the styling

--- app/page.jsx
+++ app/page.jsx
@@ -24,7 +24,7 @@ export default function Home() {
           <p className="text-[#9ba5bf] font-semibold text-xl tracking-wide">
             Freedom to move crypto anywhere
           </p>
-          <Link href={''} className="flex items-center space-x-4">
+          <Link href={""} className="flex items-center space-x-4">
             <span className="flex items-center p-[10px] rounded-lg bg-[#e32474] space-x-4">
               <button className="font-semibold">Lanuch App </button>
               <FaChevronRight />
@@ -33,16 +33,22 @@ export default function Home() {
           </Link>
         </section>
 
-        <form className="container mx-auto bg-[#0c1526] w-[28%] p-6 rounded-xl mt-10 border border-white border-opacity-10">
+        <form className="container mx-auto bg-[#0c1526] w-[28%] p-6 rounded-xl mt-10 border border-white border-opacity-10 hover:translate-y-[-25px] transform transition duration-700 ease-in-out">
           <section>
             <div className="flex flex-col space-y-7 relative">
               <div className="bg-[#111c36] rounded-md py-4 px-3 space-y-1">
                 <p className="text-base text-white opacity-60">From</p>
                 <div className="flex justify-between space-x-2 w-[100%] bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
-                  <input type="text" placeholder="Loopring" disabled className="bg-transparent" />
+                  <input
+                    type="text"
+                    placeholder="Loopring"
+                    disabled
+                    className="bg-transparent"
+                  />
                   <FaChevronDown />
                 </div>
               </div>
+
               <div className="absolute left-1/2 top-[39%] transform -translate-x-1/2 -translate-y-1/2 border-white border border-opacity-20 rounded-full">
                 <div className="bg-[#0c1526] rounded-full p-1">
                   <div className="border border-white border-opacity-[0px] rounded-full p-1">
@@ -50,7 +56,9 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <div className="bg-[#111c36] rounded-md py-4 px-3 space-y-1">
+
+              {/* Content that will move up on hover */}
+              <div className="content-move-up bg-[#111c36] rounded-md py-4 px-3 space-y-1 transition-transform duration-300 ease-in-out">
                 <p className="text-base text-white opacity-60">To</p>
                 <div className="flex justify-end space-x-2 w-[100%] bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
                   <FaChevronDown />
