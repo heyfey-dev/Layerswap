43b24270e0cd146de3f07a68bffb5846ee9b8617
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-13 20:41:53 +0100

    done with the responsiveness

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,62 +4,37 @@ import layerswapLogo from "../public/layerswap_logo.png";
 export const Navbar = () => {
   return (
     <nav className="font-sans container max-w-full border-b border-[#232b3b] pb-1">
-      {/* web view */}
-      <section className="hidden md:flex items-center justify-between mx-28">
-        <div className="flex items-center ">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between md:mx-28">
+        <div className="flex items-center pl-2 md:pl-0">
           <Image
             src={layerswapLogo}
             alt="layer"
             height={80}
             width={80}
-            className="h-[60px] w-[60px]"
+            className="h-[50px] w-[50px] md:h-[60px] md:w-[60px]"
           />
-          <h2 className="text-[#e5e7eb] font-semibold tracking-wide text-xl">
+          <h2 className="text-[#e5e7eb] font-semibold tracking-wide text-lg md:text-xl">
             Layerswap
           </h2>
         </div>
-        <ul className="flex text-white space-x-14 font-semibold text-[17px]">
-          <li className="underline decoration-2 decoration-[#e32474] underline-offset-2">
-            V8 Alpha
-          </li>
-          <li>Testnet</li>
-          <li>Explorer</li>
-          <li>API</li>
-        </ul>
-      </section>
-
-      {/* mobile view */}
-      <section className="md:hidden flex flex-col">
-        <div className="flex items-center pl-2">
-          <Image
-            src={layerswapLogo}
-            alt="layer"
-            height={80}
-            width={80}
-            className="h-[50px] w-[50px]"
-          />
-          <h2 className="text-[#e5e7eb] font-semibold tracking-wide text-lg">
-            Layerswap
-          </h2>
-        </div>
-        <ul className="flex justify-evenly text-white text-opacity-90 font-semibold text-xs border-t border-[#232b3b] py-2">
-          <li className="ring-1 ring-[#6e0040] bg-[#14213d] rounded-full p-2">
+        <ul className="flex justify-evenly md:justify-start md:space-x-14 text-white text-opacity-90 font-semibold text-xs md:text-[17px] border-t md:border-t-0 border-[#232b3b] py-2 md:py-0">
+          <li className="ring-1 md:ring-0 ring-[#6e0040] bg-[#14213d] md:bg-transparent rounded-full md:rounded-none p-2 md:p-0 md:border-b-2 md:border-[#e32474]">
             <button>V8 Alpha</button>
           </li>
-          <li className="ring-1 ring-white ring-opacity-20 bg-[#14213d] rounded-full p-2">
+          <li className="ring-1 md:ring-0 ring-white ring-opacity-20 bg-[#14213d] md:bg-transparent rounded-full p-2 md:p-0">
             <button>Explorer</button>
           </li>
-          <li className="ring-1 ring-white ring-opacity-20 bg-[#14213d] rounded-full p-2">
+          <li className="ring-1 md:ring-0 ring-white ring-opacity-20 bg-[#14213d] md:bg-transparent rounded-full p-2 md:p-0 md:hidden">
             <button>Help Center</button>
           </li>
-          <li className="ring-1 ring-white ring-opacity-20 bg-[#14213d] rounded-full p-2">
+          <li className="ring-1 md:ring-0 ring-white ring-opacity-20 bg-[#14213d] md:bg-transparent rounded-full p-2 md:p-0">
             <button>Testnet</button>
           </li>
-          <li className="ring-1 ring-white ring-opacity-20 bg-[#14213d] rounded-full p-2">
+          <li className="ring-1 md:ring-0 ring-white ring-opacity-20 bg-[#14213d] md:bg-transparent rounded-full p-2 md:p-0">
             <button>API</button>
           </li>
         </ul>
-      </section>
+      </div>
     </nav>
   );
 };
