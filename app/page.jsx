fe9c9d27f4499392722a9d4a36778bed757f190c
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-13 16:51:34 +0100

    done with the responsiveness of the form

--- app/page.jsx
+++ app/page.jsx
@@ -9,23 +9,23 @@ export default function Home() {
     <main className="font-sans bg-[#0b1124] h-[100vh] w-full overflow-y-hidden">
       <Navbar />
 
-      <div className="mt-16">
-        <section className="flex flex-col items-center text-white space-y-3">
-          <h1 className="text-6xl w-[35%] font-bold">
-            <span className="underline decoration-4 decoration-[#e32474] underline-offset-2">
+      <div className="mt-10 md:mt-16">
+        <section className="flex flex-col ml-5 md:ml-0 items-start md:items-center text-white space-y-3">
+          <h1 className="text-3xl md:text-6xl font-bold flex flex-wrap justify-start md:justify-center max-w-[600px] mx-auto tracking-normal md:tracking-wide">
+            <span className="underline decoration-2 md:decoration-4 decoration-[#e32474] underline-offset-2 mr-2">
               Fast
-            </span>{" "}
-            and{" "}
-            <span className="underline decoration-4 decoration-[#e32474] underline-offset-2">
-              reliable{" "}
             </span>
-            crypto transfers
+            <span className="mr-2">and</span>
+            <span className="underline decoration-2 md:decoration-4 decoration-[#e32474] underline-offset-2 mr-2">
+              reliable
+            </span>
+            <span>crypto transfers</span>
           </h1>
-          <p className="text-[#9ba5bf] font-semibold text-xl tracking-wide">
+          <p className="text-[#9ba5bf] font-semibold text-base md:text-xl tracking-wide">
             Freedom to move crypto anywhere
           </p>
-          <Link href={'/subpage'} className="flex items-center space-x-4">
-            <span className="flex items-center p-[10px] rounded-lg bg-[#e32474] space-x-4">
+          <Link href={'/subpage'} className="flex items-center space-x-4 text-[15px] md:text-base">
+            <span className="flex items-center p-[10px] rounded-xl bg-[#e32474] space-x-4">
               <button className="font-semibold">Lanuch App </button>
               <FaChevronRight />
             </span>
@@ -33,19 +33,19 @@ export default function Home() {
           </Link>
         </section>
 
-        <form className="container mx-auto bg-[#0c1526] w-[28%] p-6 rounded-xl mt-10 border border-white border-opacity-10 hover:translate-y-[-25px] transform transition duration-700 ease-in-out cursor-pointer">
+        <form className="container mx-auto bg-[#0c1526] w-full max-w-xs sm:max-w-sm md:max-w-md p-6 rounded-xl mt-12 md:mt-10 border border-white border-opacity-10 md:hover:translate-y-[-25px] transform transition duration-700 ease-in-out cursor-pointer">
           <section>
             <div className="flex flex-col space-y-7 relative">
               <div className="bg-[#111c36] rounded-md py-4 px-3 space-y-1">
                 <p className="text-base text-white opacity-60">From</p>
-                <Link href={'/subpage'} className="flex justify-between space-x-2 w-[100%] bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
+                <Link href={'/subpage'} className="flex justify-between space-x-2 w-full bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
                   <input
                     type="text"
                     placeholder="Loopring"
                     disabled
-                    className="bg-transparent"
+                    className="bg-transparent w-full"
                   />
-                  <FaChevronDown />
+                  <FaChevronDown className="flex-shrink-0" />
                 </Link>
               </div>
 
@@ -57,10 +57,16 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className="content-move-up bg-[#111c36] rounded-md py-4 px-3 space-y-1 transition-transform duration-300 ease-in-out">
+              <div className="bg-[#111c36] rounded-md py-4 px-3 space-y-1 md:transition-transform duration-300 ease-in-out">
                 <p className="text-base text-white opacity-60">To</p>
-                <div className="flex justify-end space-x-2 w-[100%] bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
-                  <FaChevronDown />
+                <div className="flex justify-between space-x-2 w-full bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
+                  <input
+                    type="text"
+                    placeholder="Select network"
+                    disabled
+                    className="bg-transparent w-full"
+                  />
+                  <FaChevronDown className="flex-shrink-0" />
                 </div>
               </div>
             </div>
