e7f35f6fcbf1aaea0ee9b69ac09980b37fc4dec0
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-13 13:26:20 +0100

    made a change

--- app/components/LandingPageFooter.jsx
+++ app/components/LandingPageFooter.jsx
@@ -3,13 +3,13 @@ import { FaDiscord, FaGithub, FaYoutube } from "react-icons/fa6";
 
 export const LandingPageFooter = () => {
   return (
-    <footer className="bg-[#0b1124] font-sans fixed bottom-0 left-0 right-0 flex items-center justify-around py-5">
+    <footer className="bg-[#0b1124] font-sans fixed bottom-0 left-0 right-0 flex items-center justify-around py-5 px-5">
         <p className="text-white text-opacity-30 text-[13px]">
           © 2024 Layerswap Labs, Inc. All rights reserved.
         </p>
       <ul className="flex items-center space-x-7">
         <li className="text-white text-opacity-30 text-xs">
-          Privacy Policy.
+          Privacy Policy
         </li>
         <li className="text-white text-opacity-30 text-xs">
           Terms of Services
