6d76d9c45569a93becb087a4ceb49eb8222ffe71
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-13 13:24:09 +0100

    done styling the homepage, still trying to perfect some stuff though

--- app/page.jsx
+++ app/page.jsx
@@ -1,52 +1,67 @@
 import { FaChevronDown, FaChevronRight } from "react-icons/fa6";
-import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { IoSwapVertical } from "react-icons/io5";
+import { LandingPageFooter } from "./components/LandingPageFooter";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="font-sans bg-[#0b1124] h-[100vh] w-full">
+    <main className="font-sans bg-[#0b1124] h-[100vh] w-full overflow-y-hidden">
       <Navbar />
 
-      <div>
-        <section>
-          <h1>
-            <span>Fast</span> and <span>reliable </span>crypto transfers
+      <div className="mt-16">
+        <section className="flex flex-col items-center text-white space-y-3">
+          <h1 className="text-6xl w-[35%] font-bold">
+            <span className="underline decoration-4 decoration-[#e32474] underline-offset-2">
+              Fast
+            </span>{" "}
+            and{" "}
+            <span className="underline decoration-4 decoration-[#e32474] underline-offset-2">
+              reliable{" "}
+            </span>
+            crypto transfers
           </h1>
-          <div>Freedom to move crypto anywhere</div>
-          <div>
-            <span>
-              <button>Lanuch App </button>
+          <p className="text-[#9ba5bf] font-semibold text-xl tracking-wide">
+            Freedom to move crypto anywhere
+          </p>
+          <Link href={''} className="flex items-center space-x-4">
+            <span className="flex items-center p-[10px] rounded-lg bg-[#e32474] space-x-4">
+              <button className="font-semibold">Lanuch App </button>
               <FaChevronRight />
             </span>
-            <p>Learn more</p>
-          </div>
+            <p className="font-semibold">Learn more</p>
+          </Link>
         </section>
 
-        <form className="container mx-auto bg-[#0c1526] w-[35%] p-6 rounded-md mt-5">
+        <form className="container mx-auto bg-[#0c1526] w-[28%] p-6 rounded-xl mt-10 border border-white border-opacity-10">
           <section>
-            <div className="flex flex-col space-y-1 relative">
+            <div className="flex flex-col space-y-7 relative">
               <div className="bg-[#111c36] rounded-md py-4 px-3 space-y-1">
-                <p className="text-sm text-white opacity-60">From</p>
-                <FaChevronDown />
+                <p className="text-base text-white opacity-60">From</p>
+                <div className="flex justify-between space-x-2 w-[100%] bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
+                  <input type="text" placeholder="Loopring" disabled className="bg-transparent" />
+                  <FaChevronDown />
+                </div>
               </div>
-              <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 border-white border border-opacity-20 rounded-full ">
+              <div className="absolute left-1/2 top-[39%] transform -translate-x-1/2 -translate-y-1/2 border-white border border-opacity-20 rounded-full">
                 <div className="bg-[#0c1526] rounded-full p-1">
                   <div className="border border-white border-opacity-[0px] rounded-full p-1">
-                    <IoSwapVertical className="text-white opacity-60 h-[15px] w-[15px] cursor-pointer" />
+                    <IoSwapVertical className="text-white opacity-60 h-[20px] w-[20px] cursor-pointer" />
                   </div>
                 </div>
               </div>
               <div className="bg-[#111c36] rounded-md py-4 px-3 space-y-1">
-                <p className="text-sm text-white opacity-60">To</p>
-                <FaChevronDown />
+                <p className="text-base text-white opacity-60">To</p>
+                <div className="flex justify-end space-x-2 w-[100%] bg-[#14213d] border border-white border-opacity-5 cursor-pointer py-3 px-2 text-white opacity-80 outline-none rounded-md text-opacity-60">
+                  <FaChevronDown />
+                </div>
               </div>
             </div>
           </section>
         </form>
       </div>
 
-      <Footer />
+      <LandingPageFooter />
     </main>
   );
 }
