e5a9a5337da52591610e9315d18f9c0c30e89b0c
Your New Username <danieokandeji@gmail.com>
2024-09-26 01:06:30 +0100

    new

--- app/components/TokenAssetsDropdown.jsx
+++ app/components/TokenAssetsDropdown.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const TokenAssetsDropdown = ({ assets, onSelect }) => {
   return (
-    <div className="bg-[#0c1526] border border-white border-opacity-5 absolute right-[1px] top-[54px] md:top-[54px] p-2 rounded-lg w-[150px] animate-slideDown flex flex-col items-center z-50 max-h-[100px] overflow-y-auto">
+    <div className="bg-[#0c1526] border border-white border-opacity-5 absolute right-[1px] top-[54px] md:top-[54px] p-2 rounded-lg w-[150px] animate-slideDown flex flex-col items-center z-50">
       {assets?.map((crypto, index) => (
         <button
           key={crypto.display_asset}
-          className="relative flex items-center justify-center w-full mb-3 last:mb-0 rounded-md p-2 transition-colors duration-200 group"
+          className="relative flex items-center justify-center w-full mb-2 last:mb-0 rounded-md p-1 transition-colors duration-200 group"
           onClick={() => onSelect(crypto)}
         >
           <div className="absolute inset-0 rounded-md group-hover:bg-[#1c2d4a] transition-colors duration-200"></div>
@@ -17,7 +17,7 @@ const TokenAssetsDropdown = ({ assets, onSelect }) => {
               height={24}
               src={crypto.logo}
               alt={crypto.display_asset}
-              className="w-6 h-6 rounded-full mr-2"
+              className="w-6 h-4 rounded-full mr-2"
             />
             <span className="text-[#7a839e] text-sm">
               {crypto.display_asset}
